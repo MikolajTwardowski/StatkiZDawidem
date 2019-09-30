8194ee848f5c30755c8c2f56a569158201979384
Mikolaj <twardowskimikolaj@interia.pl>
2019-09-30 21:15:54 +0200

    Commit 4
    Dodałem skrypt generujący 100x100 obiektów GameObject przypisanych do skryptu, układa je co kratkę (1m), zmienia ich nazwę a następnie dodaje do listy tych obiektów w Klasie Mapa podpiętym pod kamere

---
+++ Statki/Assets/Mikolaj/Scripts/Mapa.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mapa : MonoBehaviour
+{
+    List<GameObject> siatka = new List<GameObject>();
+    public GameObject kratkaWody;
+
+    uint wierszy = 100;
+    uint kolumn = 100;
+    Vector3 tymczasowy;
+
+
+    private void Start()
+    {
+        for(int i = 0; i < wierszy; i++)
+        {
+            for(int j = 0; j < kolumn; j++)
+            {
+                var temp = Instantiate(kratkaWody);
+                tymczasowy = new Vector3(i, 0, j);
+                temp.transform.position = tymczasowy;
+                temp.name = "Woda_" + i + "-" + j;
+                siatka.Add(temp);
+                
+            }
+
+
+        }
+
+
+
+    }
+
+}
